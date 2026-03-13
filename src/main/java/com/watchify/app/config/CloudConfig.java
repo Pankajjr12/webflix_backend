6a7b220a01272100f9f2f834c0072d3173bf51c1
Pankaj Kumar <c.thor269@gmail.com>
2026-03-13 23:31:09 +0530

    v

--- src/main/java/com/watchify/app/config/CloudConfig.java
+++ src/main/java/com/watchify/app/config/CloudConfig.java
@@ -3,7 +3,6 @@ package com.watchify.app.config;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,15 +10,6 @@ import com.cloudinary.Cloudinary;
 
 @Configuration
 public class CloudConfig {
-
-    @Value("${cloudinary.cloud-name}")
-    private String cloudName;
-
-    @Value("${cloudinary.api-key}")
-    private String apiKey;
-
-    @Value("${cloudinary.api-secret}")
-    private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
